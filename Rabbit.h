781a428b29a62f8e0231efd24fa205a3f15dec2c
SHERAPVERSLUYS <>
2022-03-30 16:20:14 +0000

    Finished WS 4

---
+++ Rabbit.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Rabbit{
+  public:
+    Rabbit();
+    void print(ostream &out);
+    void readFromUser();
+    void readFromFile(ifstream &inFile);
+    void makeinValid();
+    bool isvalid();
+  private:
+    enum {MAX_CHAR_LEN=100};
+    char name[MAX_CHAR_LEN];
+    double maxWeight;
+    int yearRecognized;
+    bool isRare;
+    bool isValid;
+};
+ 
--- RabbitDataBase.cpp
+++ RabbitDataBase.cpp
@@ -1,1 +1,52 @@
 #include "RabbitDataBase.h"
+#include "ReadUtils.h"
+using namespace std;
+
+RabbitDataBase::RabbitDataBase(){
+  numRabbits = 0;
+  read();
+}
+
+void RabbitDataBase::read() {
+    ifstream rabbitFile("rabbits.txt");
+    numRabbits = 0;
+    while(rabbitFile.peek() != EOF && numRabbits < MAX_RABBITS) {
+        rabbitArray[numRabbits].readFromFile(rabbitFile);
+        numRabbits++;
+    }
+}
+
+void RabbitDataBase::printRabbits(ostream &out, bool printIndex) {
+    for (int index = 0; index < MAX_RABBITS; index++) {
+        if (rabbitArray[index].isvalid()) {
+            if (printIndex) {
+                out << "Index " << index << ": ";
+            }
+          rabbitArray[index].print(out);
+        } 
+    }
+} 
+
+void RabbitDataBase::save() {
+    ofstream out("rabbits.txt");
+    printRabbits(out, false);
+}
+
+void RabbitDataBase::remove(){
+int index = 0;
+    do {
+        index = readInt("Enter an index to remove between 0 and 7: ");
+    }while (index < 0 || index >= MAX_RABBITS);
+    rabbitArray[index].makeinValid();
+}
+void RabbitDataBase::add(){
+for (int index = 0; index < MAX_RABBITS; index++) {
+        if (rabbitArray[index].isvalid() == false) {
+            rabbitArray[index].readFromUser();
+            return;
+        }
+    }
+    cout << "Too many rabbits! No room for more!" << endl;
+}
+
+// Interact with the database functions using a menu to choose what to do
